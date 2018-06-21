eae3c3438e4c59657c5c586913ccce29b6ab8ad2
Bovin <lebovin@qq.com>
2018-06-21 13:20:43 +0800

    init

---
+++ src/com/bovin/study/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.bovin.study;
+
+/**
+ * HelloWorld
+ *
+ * @author Bovin
+ * Create on 2018/6/21
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
